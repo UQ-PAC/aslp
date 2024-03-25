848ad95e0892dff4180cba33ec1c5367f2e2ae0b
rina <k@rina.fyi>
2024-03-25 14:54:46 +1000

    compiling fr now

--- offlineASL-cpp/main.cpp
+++ offlineASL-cpp/main.cpp
@@ -7,7 +7,8 @@
 int main(int argc, char** argv) {
   aslp::llvm_lifter_interface iface{};
   aslp::aslp_lifter<aslp::llvm_lifter_traits> lifter{iface};
-  lifter.f_A64_decoder(llvm::APInt{32, 0xe205800ULL});
+  unsigned long long adds = 0xab030041;
+  lifter.f_A64_decoder(llvm::APInt{32, adds});
 
   std::cout << "boop" << std::endl;
 }
