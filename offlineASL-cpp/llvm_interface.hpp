ab6ae009d21d86ab9bf058ae206585b04164267e
rina <k@rina.fyi>
2024-04-03 15:51:00 +1000

    implement missing registers and intrinsics for non-float ops.

--- offlineASL-cpp/llvm_interface.hpp
+++ offlineASL-cpp/llvm_interface.hpp
@@ -8,6 +8,7 @@
 #include <string>
 #include <type_traits>
 #include <memory>
+#include <variant>
 
 #include "interface.hpp"
 #include "llvm_lifter_traits.hpp"
@@ -17,10 +18,10 @@ namespace aslp {
 class llvm_lift_time_interface : virtual public lifter_interface<llvm_lifter_traits> {
 public:
   bits bits_lit(unsigned width, std::string_view str) override {
-   return llvm::APInt{width, str, (char)2};
+    return llvm::APInt{width, str, (char)2};
   }
   bits bits_zero(unsigned width) override {
-   return llvm::APInt::getZero(width);
+    return llvm::APInt::getZero(width);
   }
   bigint bigint_lit(std::string_view str) override {
     bigint x;
@@ -33,50 +34,50 @@ public:
     }
   }
   bigint bigint_zero() override {
-   return 0LL;
+    return 0LL;
   }
 
   bits extract_bits(const bits &val, bigint lo, bigint wd) override {
-   return val.extractBits(wd, lo);
+    return val.extractBits(wd, lo);
   }
   bool f_eq_bits(const bits &x, const bits &y) override {
-   return x == y;
+    return x == y;
   }
   bool f_ne_bits(const bits &x, const bits &y) override {
-   return x != y;
+    return x != y;
   }
   bits f_add_bits(const bits &x, const bits &y) override {
-   return x + y;
+    return x + y;
   }
   bits f_sub_bits(const bits &x, const bits &y) override {
-   return x - y;
+    return x - y;
   }
   bits f_mul_bits(const bits &x, const bits &y) override {
-   return x * y;
+    return x * y;
   }
   bits f_and_bits(const bits &x, const bits &y) override {
-   return x & y;
+    return x & y;
   }
   bits f_or_bits(const bits &x, const bits &y) override {
-   return x | y;
+    return x | y;
   }
   bits f_eor_bits(const bits &x, const bits &y) override {
-   return x ^ y;
+    return x ^ y;
   }
   bits f_not_bits(const bits &x) override {
-   return ~x;
+    return ~x;
   }
   bool f_slt_bits(const bits &x, const bits &y) override {
-   return x.slt(y);
+    return x.slt(y);
   }
   bool f_sle_bits(const bits &x, const bits &y) override {
-   return x.sle(y);
+    return x.sle(y);
   }
   bits f_zeros_bits(bigint n) override {
-   return llvm::APInt::getZero(n);
+    return llvm::APInt::getZero(n);
   }
   bits f_ones_bits(bigint n) override {
-   return llvm::APInt::getAllOnes(n);
+    return llvm::APInt::getAllOnes(n);
   }
   bits f_replicate_bits(const bits &x, bigint n) override {
     assert(n >= 1);
@@ -90,41 +91,66 @@ public:
    return ret;
   }
   bits f_append_bits(const bits &x, const bits &y) override {
-    auto ret = x.zext(x.getBitWidth() + y.getBitWidth());
-   ret <<= y.getBitWidth();
-   ret |= y;
-   return ret;
+    auto wd = x.getBitWidth() + y.getBitWidth();
+    auto ret = x.zext(wd);
+    ret <<= y.getBitWidth();
+    ret |= y.zext(wd);
+    return ret;
   }
   bits f_ZeroExtend(const bits &x, bigint wd) override {
-   return x.zextOrTrunc(wd);
+    return x.zextOrTrunc(wd);
   }
   bits f_SignExtend(const bits &x, bigint wd) override {
-   return x.sextOrTrunc(wd);
+    return x.sextOrTrunc(wd);
   }
   bits f_lsl_bits(const bits &x, const bits &y) override {
-   return x << y;
+    return x << y;
   }
   bits f_lsr_bits(const bits &x, const bits &y) override {
-   return x.lshr(y);
+    return x.lshr(y);
   }
   bits f_asr_bits(const bits &x, const bits &y) override {
-   return x.ashr(y);
+    return x.ashr(y);
   }
   bigint f_cvt_bits_uint(const bits &x) override {
-   return x.getZExtValue();
+    return x.getZExtValue();
   }
 
 };
 
 
 class llvm_run_time_interface : virtual public lifter_interface<llvm_lifter_traits> {
-  llvm::LLVMContext &context;
-  llvm::Module &module;
-  llvm::Function &function;
-
-  rt_label builder;
-
 protected:
+  llvm::Function &function;
+  llvm::LLVMContext &context{function.getContext()};
+  llvm::Module &module{*function.getParent()};
+
+  rt_label builder{std::make_shared<llvm::IRBuilder<>>(&function.getEntryBlock())};
+
+  llvm::Function &assertDecl{createAssertDecl(module)};
+
+  static llvm::Function& createAssertDecl(llvm::Module &module) {
+    auto& context = module.getContext();
+    auto funTy = llvm::FunctionType::get(llvm::Type::getVoidTy(context), {llvm::Type::getInt1Ty(context)}, false);
+    auto fun = module.getOrInsertFunction("llvm.assert", funTy);
+    return *llvm::cast<llvm::Function>(fun.getCallee());
+  }
+
+  rt_global getOrInsertGlobal(unsigned count, llvm::Type *type, const std::string_view basename) {
+    if (count == 0) return {};
+
+    rt_global ret{std::make_shared<std::vector<llvm::GlobalVariable *>>(count)};
+    for (unsigned i = 0; i < count; i++) {
+      std::string name{basename};
+      if (count > 1)
+        name += std::to_string(i);
+
+      ret->at(i) = llvm::cast<llvm::GlobalVariable>(module.getOrInsertGlobal(name, type));
+    }
+    assert(ret->size() == count);
+    return ret;
+  }
+
   llvm::Type* intty(unsigned width) {
     return llvm::Type::getIntNTy(context, width);
   }
@@ -132,37 +158,47 @@ protected:
   bigint int_expr(rt_expr x) {
     return llvm::cast<llvm::ConstantInt>(x)->getSExtValue();
   }
+  rt_expr int_const(unsigned wd, bigint x) {
+    return llvm::ConstantInt::get(intty(wd), x);
+  }
+
 
 public:
-  llvm_run_time_interface(llvm::Function &f)
-    : function{f},
-      module{*f.getParent()},
-      builder{std::make_shared<llvm::IRBuilder<>>(&f.getEntryBlock())},
-      context{f.getContext()} {}
+  llvm_run_time_interface(llvm::Function &f) : function{f} {}
 
-  rt_lexpr v_PSTATE_C() override { 
-    return builder->CreateAlloca(intty(1), 0, nullptr, "c");
+
+  rt_lexpr v_PSTATE_C() override {
+    static rt_lexpr x{getOrInsertGlobal(1, intty(1), "C")};
+    return x;
   };
-  rt_lexpr v_PSTATE_Z() override { 
-    return builder->CreateAlloca(intty(1), 0, nullptr, "z");
+  rt_lexpr v_PSTATE_Z() override {
+    static rt_lexpr x{getOrInsertGlobal(1, intty(1), "Z")};
+    return x;
   };
   rt_lexpr v_PSTATE_V() override {
-    return builder->CreateAlloca(intty(1), 0, nullptr, "v");
+    static rt_lexpr x{getOrInsertGlobal(1, intty(1), "V")};
+    return x;
   };
   rt_lexpr v_PSTATE_N() override {
-    return builder->CreateAlloca(intty(1), 0, nullptr, "n");
+    static rt_lexpr x{getOrInsertGlobal(1, intty(1), "N")};
+    return x;
   };
-  rt_lexpr v__PC() override { 
-    return builder->CreateAlloca(intty(64), 0, nullptr, "pc");
+  rt_lexpr v__PC() override {
+    static rt_lexpr x{getOrInsertGlobal(1, intty(64), "PC")};
+    return x;
   };
-  rt_lexpr v__R() override { 
-    return builder->CreateAlloca(intty(64), 0, nullptr, "r");
+  rt_lexpr v__R() override {
+    // XXX redirect some of these to FP/LR
+    static rt_lexpr x{getOrInsertGlobal(31, intty(64), "R")};
+    return x;
   };
-  rt_lexpr v__Z() override { 
-    return builder->CreateAlloca(intty(64), 0, nullptr, "z");
+  rt_lexpr v__Z() override {
+    static rt_lexpr x{getOrInsertGlobal(32, intty(128), "Z")};
+    return x;
   };
   rt_lexpr v_SP_EL0() override {
-    return builder->CreateAlloca(intty(64), 0, nullptr, "sp");
+    static rt_lexpr x{getOrInsertGlobal(1, intty(64), "SP")};
+    return x;
   };
   rt_lexpr v_FPSR() override { assert(0); };
   rt_lexpr v_FPCR() override { assert(0); };
@@ -174,7 +210,7 @@ public:
   rt_lexpr v___ExclusiveLocal() override { assert(0); };
 
 
-  rt_lexpr f_decl_bv(std::string_view name, bigint width) override { 
+  rt_lexpr f_decl_bv(std::string_view name, bigint width) override {
     return builder->CreateAlloca(intty(width), /*addrspace*/0, /*arraysize*/nullptr, name);
   }
   rt_lexpr f_decl_bool(std::string_view name) override {
@@ -197,8 +233,10 @@ public:
     return std::make_tuple(std::move(tlabel), std::move(flabel), std::move(jlabel));
   }
 
-  void f_gen_assert(rt_expr cond) override { assert(0); }
-  rt_expr f_gen_bit_lit(bits bits) override { 
+  void f_gen_assert(rt_expr cond) override {
+    builder->CreateCall(assertDecl.getFunctionType(), &assertDecl, {cond});
+  }
+  rt_expr f_gen_bit_lit(bits bits) override {
     return llvm::ConstantInt::get(intty(bits.getBitWidth()), bits);
   }
   rt_expr f_gen_bool_lit(bool b) override {
@@ -209,21 +247,50 @@ public:
   }
 
   rt_expr f_gen_load(rt_lexpr ptr) override {
-    return builder->CreateLoad(ptr->getAllocatedType(), ptr);
+    return f_gen_array_load(ptr, 0);
   }
-  void f_gen_store(rt_lexpr var, rt_expr exp) override {
-    builder->CreateStore(exp, var);
+  void f_gen_store(rt_lexpr ptr, rt_expr exp) override {
+    return f_gen_array_store(ptr, 0, exp);
   }
-  
-  rt_expr f_gen_array_load(rt_lexpr array, bigint index) override {
-    return f_gen_load(array); // TODO
+ 
+  rt_expr f_gen_array_load(rt_lexpr var, bigint index) override {
+    llvm::Value *ptr;
+    llvm::Type *ty;
+
+    if (auto global = std::get_if<rt_global>(&var); global) {
+      auto glob = global[0]->at(index);
+      ptr = glob;
+      ty = glob->getValueType();
+    } else if (auto local = std::get_if<rt_local>(&var); local) {
+      assert(index == 0 && "non-zero index into rt_local");
+      ptr = *local;
+      ty = local[0]->getAllocatedType();
+    }
+    assert(ptr && ty);
+    return builder->CreateLoad(ty, ptr);
   }
 
-  void f_gen_array_store(rt_lexpr array, bigint index, rt_expr exp) override {
-    return f_gen_store(array, exp);
+  void f_gen_array_store(rt_lexpr var, bigint index, rt_expr exp) override {
+    llvm::Value *ptr;
+
+    if (auto global = std::get_if<rt_global>(&var); global) {
+      auto glob = global[0]->at(index);
+      ptr = glob;
+    } else if (auto local = std::get_if<rt_local>(&var); local) {
+      assert(index == 0 && "non-zero index into rt_local");
+      ptr = *local;
+    }
+    assert(ptr);
+    builder->CreateStore(exp, ptr);
   }
-  void f_gen_Mem_set(rt_expr ptr, rt_expr width, rt_expr acctype, rt_expr exp) override { assert(0); }
-  rt_expr f_gen_Mem_read(rt_expr ptr, rt_expr width, rt_expr acctype) override { assert(0); }
+  void f_gen_Mem_set(rt_expr addr, rt_expr width, rt_expr acctype, rt_expr exp) override {
+    auto ptr = builder->CreateIntToPtr(addr, llvm::PointerType::get(context, 0));
+    builder->CreateStore(exp, ptr);
+  }
+  rt_expr f_gen_Mem_read(rt_expr addr, rt_expr width, rt_expr acctype) override {
+    auto ptr = builder->CreateIntToPtr(addr, llvm::PointerType::get(context, 0));
+    return builder->CreateLoad(intty(int_expr(width)), ptr);
+  }
   void f_gen_AArch64_MemTag_set(rt_expr address, rt_expr acctype, rt_expr value) override { assert(0); }
 
   void f_AtomicStart() override { assert(0); }
@@ -233,6 +300,7 @@ public:
     return bits;
   }
   rt_expr f_gen_cvt_bool_bv(rt_expr e) override {
+    assert(e->getType()->getIntegerBitWidth() == 1 && "f_gen_cvt_bool_bv given non-bv1 expression");
     return e;
   }
 
@@ -245,7 +313,9 @@ public:
   rt_expr f_gen_or_bool(rt_expr x, rt_expr y) override {
     return f_gen_or_bits(x, y);
   }
-  rt_expr f_gen_eq_enum(rt_expr x, rt_expr y) override { assert(0); }
+  rt_expr f_gen_eq_enum(rt_expr x, rt_expr y) override {
+    return f_gen_eq_bits(x, y);
+  }
 
   rt_expr f_gen_not_bits(rt_expr x) override {
     return builder->CreateNot(x);
@@ -269,26 +339,76 @@ public:
   rt_expr f_gen_add_bits(rt_expr x, rt_expr y) override {
     return builder->CreateAdd(x, y);
   }
-  rt_expr f_gen_sub_bits(rt_expr x, rt_expr y) override { assert(0); }
-  rt_expr f_gen_sdiv_bits(rt_expr x, rt_expr y) override { assert(0); }
-  rt_expr f_gen_sle_bits(rt_expr x, rt_expr y) override { assert(0); }
-  rt_expr f_gen_slt_bits(rt_expr x, rt_expr y) override { assert(0); }
-  rt_expr f_gen_mul_bits(rt_expr x, rt_expr y) override { assert(0); }
+  rt_expr f_gen_sub_bits(rt_expr x, rt_expr y) override {
+    return builder->CreateSub(x, y);
+  }
+  rt_expr f_gen_sdiv_bits(rt_expr x, rt_expr y) override {
+    return builder->CreateSDiv(x, y);
+  }
+  rt_expr f_gen_sle_bits(rt_expr x, rt_expr y) override {
+    return builder->CreateICmpSLE(x, y);
+  }
+  rt_expr f_gen_slt_bits(rt_expr x, rt_expr y) override {
+    return builder->CreateICmpSLT(x, y);
+  }
+  rt_expr f_gen_mul_bits(rt_expr x, rt_expr y) override {
+    return builder->CreateMul(x, y);
+  }
 
-  rt_expr f_gen_append_bits(rt_expr x, rt_expr y) override { assert(0); }
-  rt_expr f_gen_lsr_bits(rt_expr x, rt_expr y) override { assert(0); }
+  rt_expr f_gen_append_bits(rt_expr x, rt_expr y) override {
+    unsigned xwd = x->getType()->getIntegerBitWidth();
+    unsigned wd = xwd + y->getType()->getIntegerBitWidth();
+    // shift lhs
+    x = builder->CreateZExtOrBitCast(x, intty(wd));
+    x = builder->CreateShl(x, int_const(wd, xwd));
+    // union with rhs
+    y = builder->CreateZExtOrBitCast(y, intty(wd));
+    return builder->CreateOr(x, y);
+  }
+  rt_expr f_gen_lsr_bits(rt_expr x, rt_expr y) override {
+    // assert(x->getType() == y->getType());
+    auto wd = x->getType()->getIntegerBitWidth();
+    auto max = llvm::ConstantInt::get(x->getType(), wd-1);
+    auto ok = builder->CreateICmpULE(y, max);
+    auto shift = builder->CreateLShr(x, y);
+    auto zero = int_const(wd, 0);
+    return builder->CreateSelect(ok, shift, zero);
+  }
 
   rt_expr f_gen_lsl_bits(rt_expr x, rt_expr y) override {
+    // assert(x->getType() == y->getType());
     auto wd = x->getType()->getIntegerBitWidth();
     auto max = llvm::ConstantInt::get(x->getType(), wd-1);
     auto ok = builder->CreateICmpULE(y, max);
     auto shift = builder->CreateShl(x, y);
-    auto zero = llvm::ConstantInt::get(x->getType(), 0);
+    auto zero = int_const(wd, 0);
     return builder->CreateSelect(ok, shift, zero);
   }
 
-  rt_expr f_gen_asr_bits(rt_expr x, rt_expr y) override { assert(0); }
-  rt_expr f_gen_replicate_bits(rt_expr x, rt_expr y) override { assert(0); }
+  rt_expr f_gen_asr_bits(rt_expr x, rt_expr y) override {
+    assert(x->getType() == y->getType());
+    auto wd = x->getType()->getIntegerBitWidth();
+    auto max = llvm::ConstantInt::get(x->getType(), wd-1);
+    auto ok = builder->CreateICmpULE(y, max);
+    auto shift = builder->CreateAShr(x, y);
+    auto zero = int_const(wd, 0);
+    return builder->CreateSelect(ok, shift, zero);
+  }
+
+  rt_expr f_gen_replicate_bits(rt_expr x, rt_expr y) override {
+    auto count = int_expr(y);
+    auto basewd = x->getType()->getIntegerBitWidth() ;
+    auto finalwd = basewd * count;
+    auto finalty = intty(finalwd);
+
+    auto base = builder->CreateZExtOrBitCast(x, finalty);
+    auto ret = base;
+    for (unsigned i = 1; i < count; i++) {
+      auto shifted = builder->CreateShl(base, int_const(finalwd, basewd * i));
+      ret = builder->CreateOr(ret, shifted);
+    }
+    return ret;
+  }
 
   rt_expr f_gen_ZeroExtend(rt_expr x, rt_expr y) override {
     return builder->CreateZExtOrBitCast(x, intty(int_expr(y)));
--- offlineASL-cpp/main.cpp
+++ offlineASL-cpp/main.cpp
@@ -1,13 +1,15 @@
 #include <iostream>
 
 #include "llvm/ADT/ArrayRef.h"
+#include "llvm/IR/Module.h"
+#include "llvm/IR/Verifier.h"
 
 #include "aslp_lifter.hpp"
 #include "llvm_interface.hpp"
 
 int main(int argc, char **argv) {
   llvm::LLVMContext context;
-  auto module = llvm::Module{"aslp_lifter_module", context};
+  llvm::Module module{"aslp_lifter_module", context};
 
   auto funty = llvm::FunctionType::get(llvm::Type::getVoidTy(context), {}, false);
   auto function = llvm::Function::Create(funty, llvm::GlobalValue::LinkageTypes::ExternalLinkage, "aslp_lifter_fun", module);
@@ -16,12 +18,17 @@ int main(int argc, char **argv) {
   aslp::llvm_lifter_interface iface{*function};
   aslp::aslp_lifter<aslp::llvm_lifter_traits> lifter{iface};
 
-  unsigned long long adds = 0xab030041;
+  assert(argc > 1);
+  char* endp;
+  unsigned long long adds = std::strtoull(argv[1], &endp, 0);
+  assert(endp == argv[1] + strlen(argv[1]));
   auto v_enc = llvm::APInt{32, adds};
   lifter.f_A64_decoder(v_enc);
 
   module.print(llvm::errs(), nullptr);
   // module.dump();
 
-  std::cout << "boop" << std::endl;
+  assert(llvm::verifyModule(module));
+
+  std::cout << "boop! module validated." << std::endl;
 }
