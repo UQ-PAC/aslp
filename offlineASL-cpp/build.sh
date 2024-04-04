4ae547b3e5b0556a64d458c6bfffc162478f0a3b
Kait Lam <k@rina.fyi>
2024-04-04 18:59:52 +1000

    Update build.sh

--- offlineASL-cpp/build.sh
+++ offlineASL-cpp/build.sh
@@ -7,7 +7,8 @@ dir="$(dirname "$0")"
 cd "$dir"
 echo ":gen A64 aarch64.+ cpp ./subprojects" | dune exec asli
 
-meson setup --reconfigure build
+rm -rfv build
+meson setup build
 pushd build
 meson compile
 DESTDIR=$(pwd)/prefix meson install
