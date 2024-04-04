7e42f4df284453c13caea66df6b28371ab0103b4
rina <k@rina.fyi>
2024-04-04 18:34:40 +1000

    ci

--- offlineASL-cpp/build.sh
+++ offlineASL-cpp/build.sh
@@ -5,9 +5,9 @@ set -o pipefail
 # echo ":gen A64 aarch64_integer_arithmetic.+ cpp $(pwd)" | dune exec asli
 dir="$(dirname "$0")"
 cd "$dir"
-# echo ":gen A64 aarch64.+ cpp ./subprojects" | dune exec asli
+echo ":gen A64 aarch64.+ cpp ./subprojects" | dune exec asli
 
-CXX=$(which clang++) CXX_LD=$(which ld.lld) meson setup --reconfigure build
+meson setup --reconfigure build
 pushd build
 meson compile
 DESTDIR=$(pwd)/prefix meson install
