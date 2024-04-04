1dfc330d0b568e71536c4744fafdd61784e39eda
rina <k@rina.fyi>
2024-04-04 16:45:22 +1000

    compile speedups with clang+lld

--- offlineASL-cpp/build.sh
+++ offlineASL-cpp/build.sh
@@ -7,7 +7,7 @@ dir="$(dirname "$0")"
 cd "$dir"
 # echo ":gen A64 aarch64.+ cpp ./subprojects" | dune exec asli
 
-meson setup --reconfigure build
+CXX=$(which clang++) CXX_LD=$(which ld.lld) meson setup --reconfigure build
 pushd build
 meson compile
 DESTDIR=$(pwd)/prefix meson install
