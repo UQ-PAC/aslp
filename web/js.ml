654a150d34045cf45627071e6f7de500b4aae108
rina <k@rina.fyi>
2024-06-24 11:07:39 +1000

    Revert "initialising jsoo"
    This reverts commit 7db74e265216a9554047dc7f87127d83fdd26ec7.

--- web/js.ml
+++
@@ -1,4 +0,0 @@
-let () =
-  print_endline "hello from js";
-  print_endline @@ Int.to_string @@ List.length LibASLResources.Res.Sites.aslfiles
-  (* print_endline @@ LibASL.Value.(pp_value @@ from_bitsLit "100") *)
